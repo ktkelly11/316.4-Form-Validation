2599e2c11b3e643b57f2273138cc5f87d09e8ae0
Katherine Kelly <ktkelly11@gmail.com>
2024-04-30 16:23:04 -0400

    Initializing files and moving to VSCode from codesandbox

---
+++ script.js
@@ -0,0 +1,9 @@
+// Part 3
+// Number 1
+// Set Up
+const registrationForm = document.getElementById("registration");
+const username = document.querySelector("username");
+const email = document.querySelector("email");
+const password = document.getElementsByClassName("password");
+// const termsAndConditions =
+// Username Validation
