ea8a1918a120a6aaeae49417453152e77cbe30b6
Katherine Kelly <ktkelly11@gmail.com>
2024-05-01 16:47:33 -0400

    username validation finished

--- script.js
+++ script.js
@@ -4,11 +4,25 @@ const registrationForm = document.getElementById("registration");
 const username = document.querySelector("username");
 const email = document.querySelector("email");
 const password = document.querySelector("password");
+let specialChars = "!@#$%^&*()-_=+[{]};:'\",<.>/?\\|";
 
 // Number 1
 // Username Validation
 function usernameValidation(username) {
   if (username === "") {
     alert("You need to fill in a username");
+    return false;
   }
+  if (username.length < 4) {
+    alert("Your username must be at least 4 characters long");
+    return false;
+  }
+  if (username.specialChars < 2) {
+    alert(
+      `"Your username must contain at least one of these unique characters: " + ${specialChars}`
+    );
+    return false;
+  }
+  alert(`Your username is ${username}`);
+  return true;
 }
