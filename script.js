599ced7a78a8056159680396185e60a6fb3d9208
Katherine Kelly <ktkelly11@gmail.com>
2024-05-01 15:44:49 -0400

    Variable declaration

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // Part 3
-// Number 1
 // Set Up
 const registrationForm = document.getElementById("registration");
 const username = document.querySelector("username");
 const email = document.querySelector("email");
-const password = document.getElementsByClassName("password");
+const password = document.querySelector("password");
 // const termsAndConditions =
 // Username Validation
