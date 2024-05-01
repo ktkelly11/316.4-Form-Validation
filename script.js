d60dec4f2aec6746ac3392b77aa542da04454f1c
Katherine Kelly <ktkelly11@gmail.com>
2024-05-01 16:12:08 -0400

    working on username validation

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ const registrationForm = document.getElementById("registration");
 const username = document.querySelector("username");
 const email = document.querySelector("email");
 const password = document.querySelector("password");
-// const termsAndConditions =
+
+// Number 1
 // Username Validation
+function usernameValidation(username) {
+  if (username === "") {
+    alert("You need to fill in a username");
+  }
+}
